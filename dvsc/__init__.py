29120366a0d7891ac33d6ccaddd97f52e80c4231
Yuval <yuvalgal51@gmail.com>
2023-12-01 18:42:56 +0200

    add basic file structure from flask tutorial

---
+++ dvsc/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+
+def create_app():
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev"
+    )
+
+    app.config.from_pyfile("config.py", silent=True)
+
+    # apply the blueprints to the app
+    from . import auth
+    from . import blog
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
+
+    return app
